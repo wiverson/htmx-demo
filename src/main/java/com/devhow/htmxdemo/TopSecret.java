723eea63fb7440d62cf82b1c26f2cb507cea6fdd
Will Iverson <wiverson@gmail.com>
2021-06-07 16:04:32 -0700

    Change to default private url, add now to model

--- src/main/java/com/devhow/htmxdemo/TopSecret.java
+++ src/main/java/com/devhow/htmxdemo/TopSecret.java
@@ -1,16 +1,19 @@
 package com.devhow.htmxdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @Controller
-@RequestMapping("/private/top-secret")
+@RequestMapping("/private/")
 public class TopSecret {
 
     @GetMapping("/")
-    public String index()
-    {
+    public String index(Model model) {
+        model.addAttribute("now", new Date());
         return "private-index";
     }
 
