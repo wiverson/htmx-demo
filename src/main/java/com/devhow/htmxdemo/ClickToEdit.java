2312960e3c20ff99cd456789a1aecd7439824508
Will Iverson <wiverson@gmail.com>
2021-08-23 13:53:03 -0700

    Feed the id back to the template.

--- src/main/java/com/devhow/htmxdemo/ClickToEdit.java
+++ src/main/java/com/devhow/htmxdemo/ClickToEdit.java
@@ -3,6 +3,7 @@ package com.devhow.htmxdemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,8 +22,9 @@ public class ClickToEdit {
     }
 
     @PostMapping("/edit/{id}")
-    public String editForm(Contact contact, Model model) {
+    public String editForm(Contact contact, Model model, @PathVariable String id) {
         model.addAttribute("contact", contact);
+        model.addAttribute("id", id);
         return "click-to-edit-form";
     }
 
